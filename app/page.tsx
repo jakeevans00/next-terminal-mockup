4742a5a1f6e74b8955b86247354dbb8648e81be9
jakeevans00 <ejake370@gmail.com>
2024-07-27 20:40:46 -0600

    Fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Navbar from "./_components/Navbar";
 import Terminal from "@/app/_components/Terminal";
-import FileTree from "./_components/FileTree";
-import fileTreeData from "./_lib/FileTreeData";
-import Linux from "./_components/Linux";
+import FileTree from "@/app/_components/FileTree";
+import fileTreeData from "@/app/_lib/FileTreeData";
+import Linux from "@/app/_components/Linux";
 
 export default function Home() {
   return (
